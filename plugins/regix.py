57aad953b01b602f208bcf46e9b91a250f5c3438
Thanuj <39941386+THANOOJ666@users.noreply.github.com>
2025-06-19 10:52:52 +0530

    Update regix.py

--- plugins/regix.py
+++ plugins/regix.py
@@ -57,7 +57,7 @@ async def pub_(bot, message):
     await db.add_frwd(user)
     await send(client, user, "<b>🚥 ғᴏʀᴡᴀʀᴅɪɴɢ sᴛᴀʀᴛᴇᴅ via <a href=https://t.me/Jisshu_forward_bot>Forward Bot</a></b>")
     sts.add(time=True)
-    sleep = 1 if _bot['is_bot'] else 10
+    sleep = 1 if _bot['is_bot'] else 3
     await msg_edit(m, "<b>Processing...</b>") 
     temp.IS_FRWD_CHAT.append(i.TO)
     temp.lock[user] = locked = True
@@ -96,7 +96,7 @@ async def pub_(bot, message):
                         or completed <= 100): 
                       await forward(client, MSG, m, sts, protect)
                       sts.add('total_files', notcompleted)
-                      await asyncio.sleep(10)
+                      await asyncio.sleep(3)
                       MSG = []
                 else:
                    new_caption = custom_caption(message, caption)
@@ -133,7 +133,7 @@ async def copy(bot, msg, m, sts):
    except FloodWait as e:
      await edit(m, 'Progressing', e.value, sts)
      await asyncio.sleep(e.value)
-     await edit(m, 'Progressing', 10, sts)
+     await edit(m, 'Progressing', 3, sts)
      await copy(bot, msg, m, sts)
    except Exception as e:
      print(e)
@@ -149,7 +149,7 @@ async def forward(bot, msg, m, sts, protect):
    except FloodWait as e:
      await edit(m, 'Progressing', e.value, sts)
      await asyncio.sleep(e.value)
-     await edit(m, 'Progressing', 10, sts)
+     await edit(m, 'Progressing', 3, sts)
      await forward(bot, msg, m, sts, protect)
 
 PROGRESS = """
@@ -180,7 +180,7 @@ async def msg_edit(msg, text, button=None, wait=None):
         
 async def edit(msg, title, status, sts):
    i = sts.get(full=True)
-   status = 'Forwarding' if status == 10 else f"Sleeping {status} s" if str(status).isnumeric() else status
+   status = 'Forwarding' if status == 3 else f"Sleeping {status} s" if str(status).isnumeric() else status
    percentage = "{:.0f}".format(float(i.fetched)*100/float(i.total))
    
    now = time.time()
